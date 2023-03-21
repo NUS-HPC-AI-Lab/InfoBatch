b973a9ba53414cad3b8f8f1d70303b14bc57d4d6
henryqin1997 <henryqin1997@gamil.com>
2023-03-21 15:37:08 +0800

    remove redundant space

--- cifar_example.py
+++ cifar_example.py
@@ -140,7 +140,7 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-    print('epoch:', epoch, '   Training Accuracy:', 100. * correct / total, '  Train loss:', train_loss / len(trainloader))
+    print('epoch:', epoch, '  Training Accuracy:', 100. * correct / total, '  Train loss:', train_loss / len(trainloader))
     train_acc.append(correct/total)
 
 def test(epoch):
