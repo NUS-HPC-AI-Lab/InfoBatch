80d4d38eb64a5309f9f3d1a0fd4d407d73d41ec1
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2023-04-25 12:02:38 +0800

    Update infobatch.py

--- infobatch.py
+++ infobatch.py
@@ -8,7 +8,7 @@ class InfoBatch(Dataset):
         self.ratio = ratio
         self.num_epoch = num_epoch
         self.delta = delta
-        self.scores = np.ones([len(self.dataset)])
+        self.scores = np.ones([len(self.dataset)])*3
         self.transform = dataset.transform
         self.weights = np.ones(len(self.dataset))
         self.save_num = 0
